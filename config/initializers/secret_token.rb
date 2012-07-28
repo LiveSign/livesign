63b4c3c2fe19e44851e1c2baa0276dda93cf316b
Damien Wilson <damien@mindglob.com>
2012-07-28 13:55:26 -0700

    Stub out a rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LiveSign::Application.config.secret_token = 'f9410b2a99a473c2f0527e5c9fc06e093e2fb1919ccd8d68ced78bd1da9d1a378ea6069868a3f7fa1182f00a7999fe4fcdd023311cbd8e7cc07b5a3ded1055b7'
