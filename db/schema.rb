8230e9490e9c612218679c02ba1c5ec1d0e5b19f
Damien Wilson <damien@mindglob.com>
2012-07-28 15:07:44 -0700

    Create schema from migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,35 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20120728215148) do
+
+  create_table "propositions", :force => true do |t|
+    t.text     "title"
+    t.text     "overview"
+    t.text     "full"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "signatures", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "proposition_id"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "phone"
+    t.string   "street_address"
+    t.string   "email_address"
+    t.string   "city"
+    t.string   "state"
+    t.string   "name"
+    t.string   "zip_code"
+    t.string   "tos_acceptance"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
 
 end
