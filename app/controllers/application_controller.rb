cf4d2cb8d47765ee515c7efe8a6c6e2a1285bddd
Damien Wilson <damien@mindglob.com>
2012-07-28 21:42:56 -0700

    Add OpenTok session helpers to ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # Retrieve a cached instance of an OpenTok SDK client or intialize a new one if we haven't already.
+  # @return [OpenTok::OpenTokSDK] An OpenTokSDK instance
+  def opentok
+    @opentok ||= OpenTok::OpenTokSDK.new(Rails.configuration.opentok.api_key, Rails.configuration.opentok.api_secret)
+  end
+
+  # Generate a new token for a new session for OpenTok.
+  # @param [Hash<Symbol>] opts A hash of options
+  # @option opts [String] :metadata A string of metadata to be stored with the OpenTok session
+  # @return [String] An OpenTok authentication token
+  def opentok_token(opts = {})
+    token_options = {
+      session_id: opentok.create_session(request.remote_addr),
+      role: OpenTok::RoleConstants::PUBLISHER
+    }
+
+    options[:metadata] = opts[:metadata] if opts[:metadata]
+
+    opentok.generate_token(token_options)
+  end
 end
