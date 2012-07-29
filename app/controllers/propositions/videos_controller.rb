ef2c0ed515046d5b3ce6024b3a53969d62627bbf
Damien Wilson <damien@mindglob.com>
2012-07-29 07:06:01 -0700

    Resolved merge conflicts in routes file.

--- app/controllers/propositions/videos_controller.rb
+++ app/controllers/propositions/videos_controller.rb
@@ -2,6 +2,9 @@ class Propositions::VideosController < Propositions::BaseController
 
   before_filter :get_proposition, :get_user, :get_signature
 
+  def index
+  end
+
   def new
     @client_config = { opentok_api_key: Rails.configuration.opentok.api_key }
     @client_config[:opentok_session_id] = opentok_session
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 LiveSign::Application.routes.draw do
   
-  resources :propositions, :only => [:show, :index] do
+  resources :propositions do
     resources :signatures, :except => [:destroy], :controller => 'propositions/signatures'
-    resources :users, :except => [:destroy], :controller => 'propositions/users'
-    resources :videos, :only => [:new, :create], :controller => 'propositions/videos'
+    resources :users, :only => [:new, :create], :controller => 'propositions/users'
+    resources :videos, :only => [:new, :create, :index], :controller => 'propositions/videos'
   end
 
   get '/terms_of_service' => 'pages#terms_of_service', :as => :terms_of_service
