a2016370c0ee010a39a45a347e6c66f2e0724cd1
Damien Wilson <damien@mindglob.com>
2012-07-28 21:54:46 -0700

    Clean up and refactor OpenTok application helpers.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,18 +7,19 @@ class ApplicationController < ActionController::Base
     @opentok ||= OpenTok::OpenTokSDK.new(Rails.configuration.opentok.api_key, Rails.configuration.opentok.api_secret)
   end
 
-  # Generate a new token for a new session for OpenTok.
+  # Create a new OpenTok session via the OpenTok SDK client.
+  # @return [String] An OpenTok session id
+  def opentok_session
+    opentok.create_session(request.remote_addr)
+  end
+
+  # Generate a new token for an OpenTok session.
   # @param [Hash<Symbol>] opts A hash of options
   # @option opts [String] :metadata A string of metadata to be stored with the OpenTok session
   # @return [String] An OpenTok authentication token
   def opentok_token(opts = {})
-    token_options = {
-      session_id: opentok.create_session(request.remote_addr),
-      role: OpenTok::RoleConstants::PUBLISHER
-    }
-
+    token_options = { session_id: opentok_session, role: OpenTok::RoleConstants::PUBLISHER }
     options[:metadata] = opts[:metadata] if opts[:metadata]
-
     opentok.generate_token(token_options)
   end
 end
