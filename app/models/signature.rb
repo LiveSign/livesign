f87a3268f669f710d7b742ffe2ff019ca126740b
Damien Wilson <damien@mindglob.com>
2012-07-29 09:39:45 -0700

    Enable access to Signature#verified.

--- app/models/signature.rb
+++ app/models/signature.rb
@@ -1,8 +1,9 @@
 class Signature < ActiveRecord::Base
   belongs_to :user
   belongs_to :proposition
+
   validates_uniqueness_of :user_id, :scope => [:proposition_id]
   validates_presence_of :user_id, :proposition_id
 
-  attr_accessible :user, :proposition, :data, :opentok_archive_id
+  attr_accessible :user, :proposition, :data, :opentok_archive_id, :verified
 end
