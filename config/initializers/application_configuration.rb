043dc825850cb2b2bcaf417329b62e68533efdcf
Damien Wilson <damien@mindglob.com>
2012-07-28 19:39:09 -0700

    Clean up app config initializer.

--- config/initializers/application_configuration.rb
+++ config/initializers/application_configuration.rb
@@ -1,18 +1,19 @@
-begin
+if File.exist? Rails.root.join("config/config.yml")
   config = YAML.load_file Rails.root.join("config/config.yml")
+else
+  raise "Unable to load configuration variables from #{config}"
+end
 
-  config[Rails.env].each do |k,v|
-    key = k + "="
-    value = if v.is_a?(Hash)
-      Hashie::Mash.new(v)
-    else
-      v
-    end
-
-    Rails.configuration.send(key.to_sym, value)
+# For each environment, find all keys and assign them to Rails configuration methods.
+# Hashes are converted in Hashe::Mash instances to allow for indifferent access; ie:
+# config[:key], config["key"], or config.key
+config[Rails.env].each do |k,v|
+  key = k + "="
+  value = if v.is_a?(Hash)
+    Hashie::Mash.new(v)
+  else
+    v
   end
 
-rescue Exception => e
-  Rails.logger.error "Unable to load configuration variables from #{config}"
-  raise e
+  Rails.configuration.send(key.to_sym, value)
 end
